382df5fbf2b8911aaeb14c8054688bea8b317ad0
cpucode <923992029@qq.com>
2020-07-23 20:09:06 +0800

    应用程序hello

--- packages/experimental/HelloAndroid/Android.mk
+++ packages/experimental/HelloAndroid/Android.mk
@@ -1,3 +1,14 @@
+ #
+ # @Author: cpu_code
+ # @Date: 2020-07-22 20:54:53
+ # @LastEditTime: 2020-07-22 21:59:01
+ # @FilePath: \android_bottom\packages\experimental\HelloAndroid\Android.mk
+ # @Gitee: https://gitee.com/cpu_code
+ # @Github: https://github.com/CPU-Code
+ # @CSDN: https://blog.csdn.net/qq_44226094
+ # @Gitbook: https://923992029.gitbook.io/cpucode
+ #
+ # 编译脚本文件
 LOCAL_PATH:= $(call my-dir)
 
 include $(CLEAR_VARS)
--- packages/experimental/HelloAndroid/src/shy/luo/hello/HelloAndroid.java
+++ packages/experimental/HelloAndroid/src/shy/luo/hello/HelloAndroid.java
@@ -1,17 +1,30 @@
+/*
+ * @Author: cpu_code
+ * @Date: 2020-07-22 20:54:53
+ * @LastEditTime: 2020-07-22 21:54:27
+ * @FilePath: \android_bottom\packages\experimental\HelloAndroid\src\shy\luo\hello\HelloAndroid.java
+ * @Gitee: https://gitee.com/cpu_code
+ * @Github: https://github.com/CPU-Code
+ * @CSDN: https://blog.csdn.net/qq_44226094
+ * @Gitbook: https://923992029.gitbook.io/cpucode/
+ */ 
 package shy.luo.hello;
 
 import android.app.Activity;
 import android.os.Bundle;
 import android.util.Log;
 
-public class HelloAndroid extends Activity {
+public class HelloAndroid extends Activity 
+{
+	// 定义了一个Activity组件HelloAndroid
 	private final static String LOG_TAG = "shy.luo.hello.HelloAndroid";
    
 	@Override
-    	public void onCreate(Bundle savedInstanceState) {
+		public void onCreate(Bundle savedInstanceState) 
+		{
         	super.onCreate(savedInstanceState);
         	setContentView(R.layout.main);
-        
+			
         	Log.i(LOG_TAG, "HelloAndroid Activity Created.");
     	}
 }
