ae6012c50e5530ce0471f4bd747355ad9616759e
cpucode <923992029@qq.com>
2020-07-26 19:46:24 +0800

    虚拟设备的应用

--- external/freg/Android.mk
+++ external/freg/Android.mk
@@ -1,6 +1,18 @@
+ #
+ # @Author: cpu_code
+ # @Date: 2020-07-22 20:55:46
+ # @LastEditTime: 2020-07-24 10:21:57
+ # @FilePath: \android_bottom\external\freg\Android.mk
+ # @Gitee: https://gitee.com/cpu_code
+ # @Github: https://github.com/CPU-Code
+ # @CSDN: https://blog.csdn.net/qq_44226094
+ # @Gitbook: https://923992029.gitbook.io/cpucode/
+ #
 LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE := freg
 LOCAL_SRC_FILES := $(call all-subdir-c-files)
+# 当前要编译的是一个可执行应用程序模块
+# 编译结果在out/target/product/gerneric/system/bin
 include $(BUILD_EXECUTABLE)
--- external/freg/freg.c
+++ external/freg/freg.c
@@ -1,3 +1,13 @@
+/*
+ * @Author: cpu_code
+ * @Date: 2020-07-22 20:55:46
+ * @LastEditTime: 2020-07-24 10:04:40
+ * @FilePath: \android_bottom\external\freg\freg.c
+ * @Gitee: https://gitee.com/cpu_code
+ * @Github: https://github.com/CPU-Code
+ * @CSDN: https://blog.csdn.net/qq_44226094
+ * @Gitbook: https://923992029.gitbook.io/cpucode/
+ */ 
 #include <stdio.h>
 #include <stdlib.h>
 #include <fcntl.h>
@@ -9,6 +19,7 @@ int main(int argc, char** argv)
 	int fd = -1;
 	int val = 0;
 
+	// 以读写方式打开设备文件/dev/freg
 	fd = open(FREG_DEVICE_NAME, O_RDWR);
 	if(fd == -1)
 	{
@@ -17,17 +28,23 @@ int main(int argc, char** argv)
 	}
 	
 	printf("Read original value:\n");
+	
+	// 读取它的内容，即读取虚拟硬件设备freg的寄存器val的内容
 	read(fd, &val, sizeof(val));
+	// 内容打印
 	printf("%d.\n\n", val);
 
 	val = 5;
 	printf("Write value %d to %s.\n\n", val, FREG_DEVICE_NAME);
-        write(fd, &val, sizeof(val));
 
-	
+	// 将一个整数5写入到虚拟硬件设备freg的寄存器val中
+    write(fd, &val, sizeof(val));
 	printf("Read the value again:\n");
-        read(fd, &val, sizeof(val));
-        printf("%d.\n\n", val);
+
+	// 将这个整数5读取
+    read(fd, &val, sizeof(val));
+	//打印出来
+    printf("%d.\n\n", val);
 
 	close(fd);
 
